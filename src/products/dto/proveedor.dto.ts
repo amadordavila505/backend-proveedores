a09d00a92818157fba07f89395120998a0c0cfab
amadordavila505 <proalexander5@gmail.com>
2023-08-30 04:54:55 -0600

    proveedore

---
+++ src/products/dto/proveedor.dto.ts
@@ -0,0 +1,22 @@
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateProveedorDto{
+
+    @IsNotEmpty()
+    @IsNumber()
+    id?: number;
+    //Los decoradores en el dto se validan por la informacion que se agreguesea la que se conecta
+
+    @IsString()
+    @IsNotEmpty()
+    proveedor: string;
+
+    @IsString()
+    @IsNotEmpty()
+    user_id: number;
+
+    @IsDate()
+    @IsOptional()
+    created_at: Date;
+
+}
